01e19805fb3a617ddc756393e4f89a40b822f585
tpokora <pokora.tomek@gmail.com>
2015-06-18 22:02:33 +0200

    fix

--- mongo..js
+++ mongo..js
@@ -2,7 +2,7 @@
  * Created by Tomek on 2015-06-18.
  */
 
-var mongodb = requier('mongodb');
+var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
 
 var url = 'mongodb://Tomek:speyside#7@ds039088.mongolab.com:39088/testdb';

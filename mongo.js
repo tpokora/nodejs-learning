e8b11e21d73e519aa2c137a9e606b7764b2249aa
tpokora <pokora.tomek@gmail.com>
2015-06-19 13:24:32 +0200

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -7,6 +7,8 @@ var MongoClient = mongodb.MongoClient;
 
 var url = 'mongodb://Tomek:pass@ds039088.mongolab.com:39088/testdb';
 
+// TODO: create user on mongolab
+
 MongoClient.connect(url, function(err, db) {
     if (err) {
         console.log('Cannot connect to database!', err);

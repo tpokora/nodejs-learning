e4d77e1db3929428173231f4515c4c02f66e0a78
tpokora <pokora.tomek@gmail.com>
2015-06-27 22:22:51 +0200

    bodyParser()

---
+++ bodyParser.js
@@ -0,0 +1,20 @@
+var connect = require('connect');
+var bodyParser = require('body-parser');
+
+// curl -d '{"username":"test"}' -H "Content-Type: application/json" http://localhost:3000
+// curl -d username=test http://localhost:3000
+
+var app = connect()
+  .use(bodyParser())
+  .use(function(req, res) {
+    res.end('New user registered: ' + req.body.username);
+  }).listen(3000);
+
+
+var app1 = connect()
+  .use(bodyParser())
+  .use(function(req, res) {
+    console.log(req.body);
+    console.log(req.files);
+    res.end('Thanks!');
+  }).listen(4000);

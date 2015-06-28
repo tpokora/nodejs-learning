0a65437b06b3a4cd7ee0aea6fdc3642bb5dcf5c0
tpokora <pokora.tomek@gmail.com>
2015-06-28 15:06:54 +0200

    fixed bodyParser method for bodyParser.js

--- bodyParser.js
+++ bodyParser.js
@@ -6,14 +6,14 @@ var bodyParser = require('body-parser');
 // curl -F image=@avg.jpg -F username=test http://localhost:4000
 
 var app = connect()
-  .use(bodyParser())
+  .use(bodyParser.json())
   .use(function(req, res) {
     res.end('New user registered: ' + req.body.username);
   }).listen(3000);
 
 
 var app1 = connect()
-  .use(bodyParser())
+  .use(bodyParser.urlencoded({ "extended": false }))
   .use(function(req, res) {
     console.log(req.body);
     console.log(req.files);

8eb68c004670abadd082ba3fe9cbb6995e6843e7
tpokora <pokora.tomek@gmail.com>
2015-07-06 21:19:27 +0200

    Saving user into Redis db

--- shoutbox/lib/user.js
+++ shoutbox/lib/user.js
@@ -9,3 +9,36 @@ function User(obj) {
     this[key] = obj[key];
   }
 }
+
+User.prototype.save = function(fn) {
+  if (this.id) {
+    this.update(fn);
+  } else {
+    var user = this;
+    db.incr('user:ids', function(err, id) {
+      if (err) {
+        return fn(err);
+      }
+      user.id = id;
+      user.hashPassword(function(err) {
+        if (err) {
+          returnfn (err);
+        }
+        user.update(fn);
+      });
+    });
+  }
+};
+
+User.prototype.update = function(fn) {
+  var user = this;
+  var id = user.id;
+  db.set('user:id:' + user.name, id, function(err) {
+    if (err) {
+      return fn(err);
+    }
+    db.hmser('user:' + id, user, function(err) {
+      fn(err);
+    })
+  })
+}
